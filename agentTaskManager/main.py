cd66e476c89c2e0ea770c9a17f5e1823a850e462
Rafael Doimo <rdoimo@lliege.com.br>
2026-05-28 16:11:59 -0300

    Add index html template

--- agentTaskManager/main.py
+++ agentTaskManager/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
+from agentTaskManager.agent import root_agent
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="agentTaskManager/templates")
+
+
+class ChatRequest(BaseModel):
+    mensagem: str
+
 
 @app.get("/")
 def home(request: Request):
@@ -11,3 +19,12 @@ def home(request: Request):
         "index.html",
         {"request": request}
     )
+
+
+@app.post("/chat")
+def chat(data: ChatRequest):
+    resposta = root_agent.run(data.mensagem)
+
+    return {
+        "resposta": str(resposta)
+    }
